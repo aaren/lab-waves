6d502d071315882ebba308ec45b2895875b7f72f
Aaron O'Leary <eeaol@leeds.ac.uk>
2012-04-07 20:00:51 +0100

    Add proc_im and join

---
+++ lab-waves/join.py
@@ -0,0 +1,150 @@
+import Image
+import os
+import glob
+import sys
+
+# A collection of tools for joining together processed images
+# join joins cam1 and cam2 images together for a single run,
+# based on the standard offset at the edge of processed lab
+# images.
+
+def simple_join(path, image, gap=0, remove_text=0):
+        print("Joining %s ..." %(path))
+        cam1 = Image.open('%s/cam1/%s' % (path, image))
+        cam2 = Image.open('%s/cam2/%s' % (path, image))
+
+        # images are 2810x690 (+/- 1 on both) with the offset 60 from the end.
+        inset = 60
+        # get the image dimensions, just in case they vary from above slightly
+        w1, h1 = cam1.size
+        w2, h2 = cam2.size
+
+        # remove text?
+        if remove_text==1:
+            cam1 = rem_text(cam1)
+        else:
+            pass
+
+        # define the crop boxes
+        box_1 = (inset, 0, w1, h1)
+        box_2 = (0, 0, w2 - inset, h2)
+
+        # crop the images
+        crop_cam1 = cam1.crop(box_1)
+        crop_cam2 = cam2.crop(box_2)
+
+        # make a new, empty image
+        w = w1 + w2 - (2 * inset) + gap
+        if h1 > h2:
+            h = h1
+        else:
+            h = h2
+        joined_image = Image.new("RGB", (w, h))
+
+        # paste the cropped images into the blank_image
+        joined_image.paste(crop_cam2, (0, 0))
+        joined_image.paste(crop_cam1, (w2 - inset + gap, 0))
+
+        return joined_image
+
+def rem_text(im):
+    """Take an image object and paste over the text areas,
+    returning an image object"""
+    w, h = im.size
+
+    top_bar = (0, 0, w, 100)
+    bottom_bar = (0, h-100, w, h)
+
+    im.paste('black', top_bar)
+    im.paste('black', bottom_bar)
+    
+    return im
+    
+
+def join(run, proc_dir):
+    path = '%s/%s' % (proc_dir, run)
+    # make a new directory for the joined images if it doesn't already exist
+    if not os.path.exists('%s/join' % path):
+        os.mkdir('%s/join' % path, 0755)
+    else:
+        print("run %s has been joined, skipping..." % run)
+        return 
+    for image in glob.glob('%s/cam1/*' % (path)):
+        image = image.split('/')[-1]
+        joined_image = simple_join(path, image)
+        # save the joined image, first creating a new directory called 'join'
+        joined_image.save('%s/join/%s' % (path, image))
+        print("...saved to %s/join/%s" % (path, image))
+
+def remove_text(run, proc_dir):
+    path = '%s/%s' % (proc_dir, run)
+
+    if not os.path.exists('%s/join_notext' % path):
+        os.mkdir('%s/join_notext' % path, 0755)
+    else:
+        print("run %s has been de-texted, skipping..." % run)
+        return 
+    
+    for image in glob.glob('%s/join/*' % (path)):
+        im = Image.open(image)
+        print("Removing text from %s" % image)
+        image = image.split('/')[-1]
+
+        outfile = '%s/join_notext/%s' % (path, image)
+
+
+        w, h = im.size
+
+        top_bar = (0, 0, w, 100)
+        bottom_bar = (0, h-100, w, h)
+
+        im.paste('black', top_bar)
+        im.paste('black', bottom_bar)
+        
+        im.save(outfile)
+
+def remove_borders(run, proc_dir):
+    path = '%s/%s' % (proc_dir, run)
+
+    if not os.path.exists('%s/join_noborder' % path):
+        os.mkdir('%s/join_noborder' % path, 0755)
+    else:
+        print("run %s has been de-bordered, skipping..." % run)
+        return 
+    
+    for image in glob.glob('%s/join/*' % (path)):
+        im = Image.open(image)
+        print("Removing borders from %s" % image)
+        image = image.split('/')[-1]
+        outfile = '%s/join_noborder/%s' % (path, image)
+
+        w, h = im.size
+        
+        box = (0, 100, w, h-100)
+        cropped = im.crop(box)
+        cropped.save(outfile)
+
+def runs(proc_dir):
+    runs = [path.split('/')[-1] for path in glob.glob('%s/*' % proc_dir)]
+    return runs
+
+def presentation(run, proc_dir):
+    """Prepares run for presentation by joining the images together
+    with a small black space between to make clear the discontinuity
+    in parallax, and with only the cam2 text shown."""
+
+    path = '%s/%s' % (proc_dir, run)
+    # make a new directory for the joined images if it doesn't already exist
+    if not os.path.exists('%s/presentation' % path):
+        os.mkdir('%s/presentation' % path, 0755)
+    else:
+        print("run %s has been joined for presentation, skipping..." % run)
+        return
+
+    for image in glob.glob('%s/cam1/*' % (path)):
+        image = image.split('/')[-1]
+        joined_image = simple_join(path, image, 50, 1)
+        # save the joined image, first creating a new directory called 'join'
+        joined_image.save('%s/presentation/%s' % (path, image))
+        print("...saved to %s/presentation/%s" % (path, image))
+
---
+++ lab-waves/proc_im.py
@@ -0,0 +1,299 @@
+## Script to process lab images
+## AOL Nov 2011
+
+from __future__ import division
+import glob
+import Image
+import ImageFont, ImageDraw
+from sys import argv
+import os
+import math
+
+
+
+# script, run = argv
+
+# Initialisation:
+#
+# list the files
+print "Initialising..."
+
+# We don't just want to operate on a single image, we want to operate on
+# many. But in the first instance, in determining the offsets for cropping
+# and checking the scale for barrel correction and determining the rotation
+# correction angle, we need only look at the first image in a given run.
+#
+# The list of runs is in the first column of the parameters file and these are
+# the same as the directory names that the images are stored in (check!!!).
+# 
+# I want to add the scale, offets and rotation angle to the parameters file.
+# Rethinking this, perhaps I should just create a new file which these data
+# are added to. They aren't scientific variables, just means to making an
+# easily useable product that I would like to note down somewhere. The new
+# file can always be tacked onto parameters if it's really necessary.
+
+def pull_col(i, tsv, delim='\t'):
+    # extract column i from a tsv file as a list
+    f = open(tsv)
+    f.seek(0)
+    lines = f.read().split('\n')
+    f.close()
+    line_entries = []
+    col = []
+    for j in range(len(lines)):
+        line_entries.append(lines[j].split(delim))
+        col.append(line_entries[j][i])
+    return col
+
+def pull_line(j, tsv, delim='\t'):
+    # pull line j from a tsv file as a list
+    f = open(tsv)
+    f.seek(0)
+    lines = f.read().split('\n')
+    f.close()
+    line = lines[j].split(delim)
+
+    return line
+
+def load_img(image):
+    im = Image.open(image)
+    im.load()
+
+    source = im.split()
+
+    red = source[0].load()
+    green = source[1].load()
+    blue = source[2].load()
+
+    w, h = im.size
+    
+    return red, green, blue, w, h
+
+def barrel_corr(image, null1):
+    # reduce the path down to just the filename
+    folder = image.split('/')[-3]
+    camera = image.split('/')[-2]
+    image = image.split('/')[-1]
+
+    cam1corr = '"0.000658776 -0.0150048 -0.00123339 1.01557914"'
+    cam2corr = '"0.023969 -0.060001 0 1.036032"'
+    
+    if camera == 'cam1':
+        corr = cam1corr
+        cam = 'cam1'
+    elif camera == 'cam2':
+        corr = cam2corr
+        cam = 'cam2'
+    else:
+        print "Camera must be cam1 or cam2"
+
+    infile = 'synced/%s/%s/%s' % (folder, cam, image)
+    outfile = 'processed/%s/%s/%s' % (folder, cam, image)
+    command = 'convert -distort Barrel %s %s %s' % (corr, infile, outfile)
+
+    print "Barrel correcting %s with %s 18mm coefficients,\n%s" % (image, cam, corr)
+    os.system(command)
+
+
+def barrel_corr_measure(run):
+    proc = []
+    proc.append(run)
+    for camera in ['cam1', 'cam2']:
+        # barrel_corr('synced/%s/%s/img_0001.jpg' % (run, camera)) 
+        # (now done externally)
+        command = 'gimp -s -f -d processed/%s/%s/img_0001.jpg &' % (run, camera)
+        os.system(command)
+
+        print "What is the rotation correction for %s? (as measured)" % camera
+        proc.append(raw_input('> '))
+
+        print "What is the offset for %s?" % camera
+        proc.append(raw_input('> '))
+
+        print "What is the position of the water surface for %s?" % camera
+        proc.append(raw_input('> '))
+
+        print "What is the position of the tank floor for %s?" % camera
+        proc.append(raw_input('> '))
+
+        if camera == 'cam1':
+            print "Where is the 25 in %s" % camera
+            proc.append(raw_input('> '))
+            print "What is the lock position?"
+            proc.append(raw_input('> '))
+            print "What is the extent of lock leakage?"
+            proc.append(raw_input('> '))
+
+        print "Weird (y/n)"
+        proc.append(raw_input('> '))
+
+    entry = ','.join(proc) + '\n'
+    f = open('proc_data', 'a')
+    f.write(entry)
+    f.close()
+
+# easy testing
+def bcm():
+    barrel_corr_measure('11_7_06g')
+
+def bc1(run):
+    for camera in ['cam1', 'cam2']:
+        barrel_corr('synced/%s/%s/img_0001.jpg' % (run, camera), None)
+
+def get_run_data(run):
+    lines = pull_col(0, 'proc_data', ',') 
+    line_number = lines.index(run)
+    line = pull_line(line_number, 'proc_data', ',')
+
+    headers = pull_line(0, 'proc_data', ',')
+    
+    run_data = {}
+
+    for header in headers:
+        index = headers.index(header)
+        run_data[header] = line[index]
+
+    return run_data
+
+def rotation_corr(image, angle):
+    # rotates the image by a given angle and saves, overwriting the original
+    im = Image.open(image)
+    rot = im.rotate(angle)
+    rot.save(image)
+
+def get_measurements(run):
+    proc1 = barrel_corr_measure('cam1', run)
+    proc2 = barrel_corr_measure('cam2', run)
+
+    proc_string = '\t'.join(proc1 + proc2)
+    print proc_string
+
+    f = open('proc_data', 'a')
+    f.write(proc_string)
+    f.close()
+
+def proc_images(proc, run, source, arg1, arg2):
+    for image in glob.glob('%s/%s/cam1/*jpg' % (source, run)):
+        print "performing %s on %s" % (proc, image)
+        proc(image, arg1)
+    for image in glob.glob('%s/%s/cam2/*jpg' % (source, run)):
+        print "performing %s on %s" % (proc, image)
+        proc(image, arg2)
+
+def rescale(image, ratio):
+    im = Image.open(image)
+    w, h = im.size
+    h_new = int(h * ratio)
+    w_new = int(w * ratio)
+    re = im.resize((w_new, h_new))
+    
+    re.save(image)
+
+def add_text(image, (scale, data)):
+    # opens and crops an image to the box given. box is a 4-tuple defining
+    # the left, upper, right and lower pixel
+    # in 15 pt Liberation Regular, "Aaron O'Leary" is 360 px wide.
+    # "University of Leeds" is 520 px wide.
+    im = Image.open(image)
+
+    ratio, run_data = scale, data
+
+    author_text = "Aaron O'Leary, University of Leeds"
+    param_a = "run %s, t=%ss: h_1 = %s, rho_0 = %s, rho_1 = %s, rho_0 = %s, "
+    param_b = "alpha = %s, D = %s" 
+    param_t = param_a + param_b
+
+    run = image.split('/')[-3]
+    param_runs = pull_col(0, 'parameters')
+    line_number = param_runs.index(run) 
+    parameter = pull_line(line_number, 'parameters')
+
+    camera = image.split('/')[-2]
+    time = int(image.split('_')[-1].split('.')[0])
+    param_text = param_t % (parameter[0], time, parameter[1], parameter[2],\
+            parameter[3], parameter[4], parameter[5], parameter[6])
+    scale_depth = 440
+
+    if camera == 'cam1':
+        cam1_ratio = ratio
+        offset = int(run_data['off_1'])
+        bottom1 = int(run_data['bottom_1'])
+
+        left = int((offset * cam1_ratio) - 60)
+        right = int((offset * cam1_ratio) + 2750)
+        upper = int((bottom1 * cam1_ratio) - (scale_depth + 100))
+        lower = int((bottom1 * cam1_ratio) + 150)
+
+    elif camera == 'cam2':
+        cam2_ratio = ratio
+        offset = int(run_data['off_2'])
+        bottom1 = int(run_data['bottom_2'])
+
+        left = int((offset * cam2_ratio) - 2750)
+        right = int((offset * cam2_ratio) + 60)
+        upper = int((bottom1 * cam2_ratio) - (scale_depth + 100))
+        lower = int((bottom1 * cam2_ratio) + 150)
+
+    box = (left, upper, right, lower)
+
+    draw = ImageDraw.Draw(im)
+    draw.rectangle((left, upper, right, upper + 100), fill = 'black')
+    draw.rectangle((left, lower - 100, right, lower), fill = 'black')
+
+    # Want to write something to join the two images together. Will probably
+    # mean splitting this function up to separate the text adding and cropping
+    # parts - hard, as this requires a change of order.
+
+    # this is platform specific!
+    fonts = '/usr/share/fonts/liberation/LiberationMono-Regular.ttf'
+    font = ImageFont.truetype(fonts, 45)
+
+    text_hi_pos = (left, upper)
+    text_low_pos = (left, lower - 100)
+
+    text_hi, text_low = param_text, author_text
+
+    draw.text(text_hi_pos, text_hi, font=font, fill="white")
+    draw.text(text_low_pos, text_low, font=font, fill="white")
+
+    cropped = im.crop(box)
+    cropped.save(image)
+
+def main(run, run_data):
+    # Barrel correct
+    proc_images(barrel_corr, run, 'synced', None, None)
+
+    # Pull the correction angles from the run data
+    theta1 = -float(run_data['rot_1'])
+    theta2 = -float(run_data['rot_2'])
+
+    # Rotation correct the images
+    proc_images(rotation_corr, run, 'processed', theta1, theta2)
+ 
+    # Resize the images to standard
+    ideal_ruler = 435
+    ruler = int(run_data['bottom_1']) - int(run_data['ruler_25'])
+    ruler_ratio = ideal_ruler / ruler
+    depth_1 = int(run_data['bottom_1']) - int(run_data['surface_1'])
+    depth_2 = int(run_data['bottom_2']) - int(run_data['surface_2'])
+    depth_ratio = depth_1 / depth_2
+    cam1_ratio = ruler_ratio
+    cam2_ratio = ruler_ratio * depth_ratio
+
+    proc_images(rescale, run, 'processed', cam1_ratio, cam2_ratio)
+    # rescaling means that the offsets, lock_pos etc. are rescaled too.
+    # Add text and crop
+    proc_images(add_text, run, 'processed', (cam1_ratio, run_data), (cam2_ratio, run_data))
+
+# MAIN SEQUENCE
+#
+runs = [path.split('/')[-1] for path in glob.glob('synced/*')]
+# runs = ['11_7_05c']
+
+#for run in runs:
+#    bc1(run)
+#for run in runs:
+#    barrel_corr_measure(run)
+for run in runs:
+    run_data = get_run_data(run)
+    main(run, run_data)
