5aef6cce137eebeeea80c84c5e00b76463bc5d72
Aaron O'Leary <eeaol@leeds.ac.uk>
2012-06-10 17:37:39 +0100

    Consolidate to main function & debug.

--- lab-waves/restruct.py
+++ lab-waves/restruct.py
@@ -16,16 +16,89 @@
 # The idea is to store things in the format rdata[cs] = [(x,z,t)...]
 # where cs means something like 'front' or 'wave1' and contains a
 # list of points, each of which is specified in x, z and t
+from __future__ import division
 from collections import namedtuple
 
-def points(arg):
+import numpy as np
+import matplotlib.pyplot as plt
+
+from aolcore import read_data
+from config import data_storage
+
+def points(arg, rdata):
     arg_list = []
     point = namedtuple(arg, 'x, z, t')
     for cam in ['cam1', 'cam2']:
         cdata = rdata[cam]
-        for f in cdata.keys()
+        for f in cdata.keys():
             arg_list += [point(e[0], e[1], int(f)) for e in cdata[f][arg]]
     return arg_list
+
+def bounding_lines(x_maxes, m_err, c_err):
+    """Given a list of (x,t), fit a straight line and return two
+    lines [m, c] displaced by error in gradient and intercept.
+    Arguments: x_maxes is a list of (x,t) points to put a line
+            through.
+            m_err, c_err are the error on the gradient and y (time)
+            intercept to be applied to the line.
+    Return:    line_up, line_down, two lines that are displaced from
+            the fitted line by given error.
+    """
+    # fit a straight line, return [m,c] for line y=mx+c
+    x, T = zip(*x_maxes)
+    m, c = np.polyfit(x, T, 1)
+    # calculate the bounding lines
+    line = namedtuple('line', 'm, c')
+    upper = line(m + m_err, c + c_err)
+    lower = line(m - m_err, c - c_err)
+    return upper, lower
+
+def get_line(arg):
+    """ Given a list of points [(x, z, t), ....], where each point is
+    represented by a named tuple, plot all of the points and prompt
+    for user selection of a line section by defining two points.
+
+    Returns list of points that fall within an error of the defined
+    line.
+    """
+    # plot the argument
+    X, Z, T = zip(*arg)
+    plt.plot(X, T, 'bo')
+    # get user input points
+    pts = plt.ginput(2,0)
+    # calculate the limits
+    hi, lo = bounding_lines(pts, 0, 0.5)
+    # select points within limits
+    def in_bounds(point, upper, lower):
+        x_lo = (point.t - upper.c) / upper.m
+        x = point.x
+        x_hi = (point.t - lower.c) / lower.m
+        return (x_lo < x < x_hi)
+
+    line = [p for p in arg if in_bounds(p, hi, lo)]
+    # plot again
+    Xs, Zs, Ts = zip(*line)
+    # xt = [(p.x, p.t) for p in line]
+    plt.plot(Xs, Ts, 'ro')
+    plt.show()
+    raw_input('')
+    plt.close()
+    return line
+
+def main(run):
+    data_file = data_storage + run
+    indata = read_data(data_file)
+    rdata = indata[run]
+    maxima = points('max', rdata)
+
+    line = get_line(maxima)
+    X = [p.x for p in line]
+    T = [p.t for p in line]
+
+    m, c = np.polyfit(X, T, 1)
+    speed = 1 / m
+
+    print "Speed is %s" % speed
 
 # TODO: Decide whether to use a tuple (x,z,t) or a dict {'x':,
 # 'z':,'t':} for each of the points.
@@ -33,19 +106,6 @@ def points(arg):
 # ANSWER: Use a named tuple. We then know which arg each point came
 # from in the first place (can we actually access this though??) and
 # the point can be accessed as an object.
-
-indata = read_data(data_file)
-rdata = indata[run]
-
-# We can then plot all of the maxima
-maxima = points('max')
-X, Z, T = zip(*maxima)
-plt.plot(X, T, 'ro')
-
-# Or indeed the maxima and the front as indistinguishable points
-all_args = points('max') + points('front')
-X, Z, T = zip(*all_args)
-plt.plot(X, T, 'bo')
 
 # and then select structures from this. This is so much shorter and
 # clearer than the other way!
@@ -68,39 +128,5 @@ plt.plot(X, T, 'bo')
 # line then the rest will follow.
 
 
-def bounding_lines(x_maxes, m_err, c_err):
-    """Given a list of (x,t), fit a straight line and return two
-    lines [m, c] displaced by error in gradient and intercept.
-    Arguments: x_maxes is a list of (x,t) points to put a line
-            through.
-            m_err, c_err are the error on the gradient and y (time)
-            intercept to be applied to the line.
-    Return:    line_up, line_down, two lines that are displaced from
-            the fitted line by given error.
-    """
-    # fit a straight line, return [m,c] for line y=mx+c
-    x, T = zip(*x_maxes)
-    m, c = np.polyfit(x, T, 1)
-    # calculate the bounding lines
-    line = namedtuple('line', 'm, c')
-    upper = line(m + m_err, c + c_err)
-    lower = line(m - m_err, c - c_err)
-    return upper, lower
-
-def get_line(arg):
-    # plot the argument
-    X, Z, T = zip(*arg)
-    plt.plot(X, T, 'bo')
-    # get user input points
-    pts = ginput(2,0)
-    # calculate the limits
-    hi, lo = bounding_lines(pts, 0, 0.5)
-    # select points within limits
-    line = [p for p in maxima \
-                        if ((p.t - hi.c) / hi.m <  p.x < (p.t - lo.c) / lo.m)]
-    # plot again
-    Xs, Zs, Ts = zip(*line)
-    plt.plot(Xs, Ts, 'ro')
-    plt.close()
-    return line
-
+# Getting subsets of all of the runs is important for dealing with
+# results. How to?
