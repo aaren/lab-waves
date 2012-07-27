9dc92674602cd7a230cab385e633e77fd8d0554f
Aaron O'Leary <eeaol@leeds.ac.uk>
2012-07-27 10:37:47 +0100

    funtional wave selection

--- lab-waves/restruct.py
+++ lab-waves/restruct.py
@@ -22,8 +22,9 @@ from collections import namedtuple
 import numpy as np
 import matplotlib.pyplot as plt
 
-from aolcore import read_data
-from config import data_storage
+from aolcore import read_data, get_parameters
+from config import data_storage, paramf
+import functions as f
 
 class Run(object):
     def __init__(self, run):
@@ -46,7 +47,7 @@ class Run(object):
 
     def load(self):
         # Explicitly load the run data
-        self.data = read_data(self.data_file)[run]
+        self.data = read_data(self.data_file)[self.index]
 
 def points(arg, rdata):
     """From a given set of run data, i.e. data[run] if extracting
@@ -113,13 +114,13 @@ def get_line(data):
     plt.plot(Xs, Ts, 'ro')
     plt.draw()
     while True:
-        print "Select bad points, middle click if none"
+        print "Select bad points, middle click if finished"
         bad = plt.ginput(0,0)
         if bad:
             # identify the bad points
             bad_points = []
             for b in bad:
-                bad_points += [p for p in line if \
+                bad_points += [p for p in data if \
                                         (b[0] - 0.1 < p.x < b[0] + 0.1) \
                                     and (b[1] - 0.5 < p.t < b[1] + 0.5)]
             # overplot
@@ -133,6 +134,31 @@ def get_line(data):
             break
         else:
             print "Indeterminate badness!"
+    while True:
+        print "Select missed points, middle click if finished"
+        missed = plt.ginput(0,0)
+        if missed:
+            missed_points = []
+            print missed
+            print ""
+            print line
+            for m in missed:
+                missed_points += [p for p in data if \
+                                        (m[0] - 0.2 < p.x < m[0] + 0.2) \
+                                    and (m[1] - 0.5 < p.t < m[1] + 0.5)]
+            Xm = [p.x for p in missed_points]
+            Tm = [p.t for p in missed_points]
+            print Xm
+            print Tm
+            plt.plot(Xm, Tm, 'co')
+            plt.draw()
+            # add missed points to line
+            for m in missed_points:
+                line.append(m)
+        elif not missed:
+            break
+        else:
+            print "I don't know who you are or where you came from."
     Xs, Ts = zip(*[(p.x, p.t) for p in line])
     plt.plot(Xs, Ts, 'yo')
     plt.draw()
@@ -161,9 +187,10 @@ def get_front(run):
     for t in range(t_max):
         # find all points in this time slice
         pts = [p for p in time_front if p.t == t]
-        # and the last three points in filtered
+        # and the last three points in filtered (have to put into
+        # list of (x,t) form for bounding_lines)
         # prepts = [p for p in filtered if (t - 3 < p.t < t)]
-        prepts = proc_front[-3:]
+        prepts = [(p.x, p.t) for p in proc_front[-3:]]
         # make the bounding lines from the previous points
         hi, lo = bounding_lines(prepts, 0, 0.5)
         for pt in pts:
@@ -173,29 +200,39 @@ def get_front(run):
                 # append the nearest point in x to the previous
                 closest = min(pts, key=lambda p: abs(prepts[-1].t - p.x))
                 proc_front.append(closest)
-    return filtered
+    return proc_front
 
 def get_lines(data, arg):
     """ From given data and argument, e.g. maxima, get a list
     of lines selected by the user.
     """
     lines = []
+    y_x, y_y = [0,1,1,0],[35,35,40,40]
+    n_x, n_y = [1,2,2,1],[35,35,40,40]
+    plt.fill(y_x, y_y, 'g')
+    plt.fill(n_x, n_y, 'r')
     while True:
         pts = points(arg, data)
+        print "Choose a line by selecting a couple of points in it."
         line = get_line(pts)
         X = [p.x for p in line]
         T = [p.t for p in line]
         m, c = np.polyfit(X, T, 1)
-        Xf = np.linspace(0,20)
+        Xf = np.linspace(0,12)
         Tf = m * Xf + c
         plt.plot(Xf, Tf)
         lines.append(line)
-        print "Enough lines?? (y/n)"
-        a = raw_input("> ")
-        if a == "y":
+        plt.draw()
+        print "Finished?? green; yes; red/anywhere; no, need more waves"
+        a = plt.ginput(1,0)
+        if (y_x[0] < a[0][0] < y_x[1]) and (y_y[0] < a[0][1] < y_y[2]):
             break
-        elif a == "n":
+        elif (n_x[0] < a[0][0] < n_x[1]) and (n_y[0] < a[0][1] < n_y[2]):
             pass
+        else:
+            # print "click in the red or green!"
+            pass
+    plt.close()
     return lines
 
 def main(run):
@@ -203,28 +240,38 @@ def main(run):
     r = Run(run)
     # explicitly load the data
     r.load()
-    # pull out the maxima from the data as a named tuple
-    maxima = points('max', r.data)
     # get the waves
-    lines = get_lines(maxima)
-    waves = {'w%s' % i: l for i,l in lines}
+    lines = get_lines(r.data, 'max')
+    waves = {'w%s' % i: l for i,l in enumerate(lines)}
     # get the front
     front = get_front(run)
-
+    print front
     # test plot this to check how separate things are
     # plot the front
     Xf = [p.x for p in front]
     Tf = [p.t for p in front]
     plt.plot(Xf, Tf, 'k*')
     # plot the waves
+    for i,w in enumerate(sorted(waves.keys())):
+        Xw = [p.x for p in waves[w]]
+        Tw = [p.t for p in waves[w]]
+        plt.plot(Xw, Tw, '*', \
+            color=plt.get_cmap('hsv')((i+1)/30))
+    print "how u lik this applz??11"
+    plt.show()
+
+if __name__ == '__main__':
+    main('r11_7_06c')
+
+
+
+
+
 
 
 
 # Getting subsets of all of the runs is important for dealing with
-# results. How to?
-# make a list of the run indices
-# indices = pull_col(0, paramf)
-# make a list of run objects
-# runs = [Run(index) for index in indices]
-# select sub groupings, e.g.
-# partial_runs = [r for r in runs if r.D == 0.4]
+# results. How to?  make a list of the run indices indices =
+# pull_col(0, paramf) make a list of run objects runs = [Run(index)
+# for index in indices] select sub groupings, e.g.  partial_runs =
+# [r for r in runs if r.D == 0.4]
