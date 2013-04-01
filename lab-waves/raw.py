717e7344980e74eaa096b9f4efacebf149ab9feb
Aaron O'Leary <aaron.oleary@gmail.com>
2013-04-02 00:10:51 +0100

    stub class for raw processing

---
+++ lab-waves/raw.py
@@ -0,0 +1,12 @@
+class RawRun(object):
+    def __init__(self, run):
+        self.index = run
+
+    def barrel_correct(self):
+        pass
+
+    def perspective_transform(self):
+        pass
+
+    def crop_text(self):
+        pass
