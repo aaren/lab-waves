2c63a40fff0730ce1a62fe5321aa10651773b913
Aaron O'Leary <aaron.oleary@gmail.com>
2013-05-15 11:41:08 +0100

    wip: incorrect bmp header.
    try running nosetests - i can't work out how to test barrel correct
    effectively. it has something to do with saving jpeg - i'm not sure that
    you can make a duplicate of a jpeg with PIL.
    
    after moving to debian wheezy, pil seems to have problems with reading
    in bmp ---> something to do with imagemagick version?

--- tests/processing_tests.py
+++ tests/processing_tests.py
@@ -2,9 +2,11 @@
 from nose.tools import *
 
 import numpy.testing as npt
+import numpy as np
 import Image
 
 from labwaves.labwaves import processing
+from labwaves.labwaves import config
 
 # Testing methods that do image processing is difficult
 # because of the necessity of creating images for state
@@ -15,13 +17,38 @@ def test_barrel_correct_version():
     pass
 
 
-# def test_barrel_correct():
-    # raw_image = 'tests/data/r11_07_06c/raw/img_0001.jpg'
-    # bc_image = 'tests/data/r11_07_06c/bc/img_0001.jpg'
-    # raw_im = Image.open(raw_image)
-    # coeffs = config.barrel_coeffs['cam1']
-    # ref_bc_im = Image.open(bc_image)
-    # test_bc_im = processing.barrel_correct(raw_im, coeffs)
-    # ref_bc_im_array = np.asarray(ref_bc_im)
-    # test_bc_im_array = np.asarray(test_bc_im)
-    # npt.assert_array_equal(ref_bc_im_array, test_bc_im_array)
+def test_barrel_correct():
+    """Wow this is a ball ache to test. The content of the image
+    changes subtley depending on whether the temporary image created
+    for convert on the command line is bmp or jpeg.
+
+    If we use bmp and have a bmp reference image, all is well.
+    However using a jpg temp and a jpg ref leads to small
+    differences.
+
+    Do we actually need to test this function???
+
+    A simple way would be to run it once and verify the output, then
+    use this as baseline for future tests. This doesn't act as an
+    independent test of the function but it does act as a sanity
+    check.
+
+    TODO: force the output of barrel_correct to be of type jpg
+
+    Difference appears to be in PIL saving the tmp file. The array
+    of this is different from the raw image array when compared.
+    Something to do with jpeg compression?? The raw file wasn't
+    created by PIL.
+
+    Ughh. Appears to be due to the quantization table for saving a
+    jpeg. Not something that can be set with PIL.
+    """
+    raw_image = 'tests/data/raw/r11_07_06c/cam1/img_0001.jpg'
+    bc_image = 'tests/data/bc1/r11_07_06c/cam1/img_0001.jpg'
+    raw_im = Image.open(raw_image)
+    coeffs = config.barrel_coeffs['cam1']
+    ref_bc_im = Image.open(bc_image)
+    test_bc_im = processing.barrel_correct(raw_im, coeffs)
+    ref_bc_im_array = np.asarray(ref_bc_im)
+    test_bc_im_array = np.asarray(test_bc_im)
+    npt.assert_array_equal(ref_bc_im_array, test_bc_im_array)
