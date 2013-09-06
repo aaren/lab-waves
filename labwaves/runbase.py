eb4cbea254bcad14814af808678c6e5e34580ed8
Aaron O'Leary <aaron.oleary@gmail.com>
2013-09-06 11:29:30 +0100

    design: InterfaceImage moved to runbase

--- labwaves/runbase.py
+++ labwaves/runbase.py
@@ -10,6 +10,7 @@ import ImageDraw
 import ImageFont
 import numpy as np
 import matplotlib.pyplot as plt
+from skimage import filter as skif
 
 from gc_turbulence.gc_turbulence.util import parallel_process, parallel_stub
 
@@ -881,9 +882,11 @@ class ProcessedRun(LabRun):
         # TODO: multiprocessing
         # TODO: runfiles should point to processed data
         for im in self.images:
-            interfaces = interface.interface(im)
-            qc_interfaces = [interface.qc(i) for i in interfaces]
-            save_interface(qc_interfaces)
+            intim = InterfaceImage(im)
+            lock_interface = intim.lock_interface
+        # possible alternate? :
+        # interfacer = InterfaceImage()
+        # lock_interface = interfacer(im, fluid_type='lock')
 
     def write_out(self, images):
         """Write images to disk.
@@ -901,3 +904,157 @@ class ProcessedRun(LabRun):
 def process_raw(image):
     """External function to allow multiprocessing raw images."""
     image.write_out()
+
+
+class InterfaceImage(object):
+# NOTE: scipy.signal._peak_finding._identify_ridge_lines may be
+# useful for wave tracking in the signal data, as well as
+# scipy.signal.find_peaks_cwt
+
+    def __init__(self, labimage):
+        self.labimage = labimage
+        self.im = self.measurement_region(labimage)
+        self.imarray = np.asarray(self.im)
+        self.pixels = np.indices(self.imarray[:, :, 0].shape)
+
+    @staticmethod
+    def measurement_region(labimage):
+        """Remove the bars from the top and bottom of a lab image."""
+        w, h = labimage.im.size
+        box = (0, config.top_bar, w, h - config.bottom_bar)
+        return labimage.im.crop(box)
+
+    @property
+    def channels(self):
+        """Return the r, g, b channels of the lab image,
+        normalised on the range [0, 1].
+        """
+        return [c.squeeze() / 255. for c in np.dsplit(self.imarray, 3)]
+
+    @property
+    def lock_fluid(self):
+        """Return the color space projection that best captures
+        the fluid from the lock.
+
+        The green channel picks it out the best as a single channel,
+        but is contaminated by background noise.
+
+        This noise is largely neutrally toned though, so we can
+        subtract one of the other channels to remove it.
+        """
+        r, g, b = self.channels
+        return r - g
+
+    def plot_channels(self):
+        """Convenience function to make a figure with the input
+        image and the three colour channels."""
+        r, g, b = self.channels
+
+        fig, ax = plt.subplots(nrows=5)
+
+        ax[0].set_title('Original')
+        ax[0].imshow(self.imarray)
+
+        ax[1].set_title('red')
+        ax[1].imshow(r)
+
+        ax[2].set_title('green')
+        ax[2].imshow(g)
+
+        ax[3].set_title('blue')
+        ax[3].imshow(b)
+
+        ax[4].set_title('r - g')
+        ax[4].imshow(self.lock_fluid)
+
+        fig.tight_layout()
+
+        return fig
+
+    @property
+    def pixel_rulers(self):
+        """Where are the rulers in pixel space"""
+        cam = self.labimage.cam
+        real_rulers = config.real_rulers[cam]
+        pixel_rulers = [[real_to_pixel(x, 0, cam)[0] for x in r]
+                        for r in real_rulers]
+
+        style = self.labimage.run.style
+        if style == 'old':
+            pass
+        elif 'new' in style:
+            # only one ruler in cam1
+            pixel_rulers = [pixel_rulers[0]]
+
+        return pixel_rulers
+
+    @property
+    def ruler_mask(self):
+        """Mask for rulers in the image."""
+        iy, ix = self.pixels
+        # Truth wherever there is a ruler
+        mask = reduce(np.logical_or, ((x1 < ix) & (ix < x2)
+                                      for x2, x1 in self.pixel_rulers))
+
+        return mask
+
+    @property
+    def bottom_mask(self):
+        """Mask the bottom 5 pixels of the image."""
+        iy, ix = self.pixels
+        mask = iy > self.im.size[1] - 5
+        return mask
+
+    @property
+    def crop_mask(self):
+        """Mask where the image has been cropped, i.e. there is black
+        background."""
+        return np.all(self.imarray < 0.01, axis=-1)
+
+    @property
+    def lock_mask(self):
+        """Combination of masks that masks out irrelevant features
+        for the lock fluid detection."""
+        return reduce(np.logical_or, (self.ruler_mask,
+                                      self.bottom_mask,
+                                      self.crop_mask,
+                                      self.lock_fluid < 0.2))
+
+    @property
+    def lock_interface(self):
+        """Pull out the lock interface."""
+        # TODO: make it only select the upper bound
+        mask = self.lock_mask
+        i = self.canny_interface(self.lock_fluid < 0.5, smooth=0, mask=~mask)
+        return i
+
+    @staticmethod
+    def canny_interface(array, sigma=5, mask=None):
+        """Perform canny edge detection on the given array, using
+        gaussian smoothing of sigma, only considering the regions
+        defined as True in mask.
+        """
+        cb = skif.canny(array, sigma=sigma, mask=mask)
+        iy, ix = np.where(cb)
+        s = ix.argsort()
+        return ix[s], iy[s]
+
+
+def test_interface():
+    fig = iim.plot_channels()
+
+    lx, ly = iim.lock_interface
+
+    for ax in fig.axes:
+        ax.plot(lx, ly, 'k.')
+
+    plt.show()
+
+# pr = runbase.ProcessedRun('r13_01_13i')
+# iim = runbase.InterfaceImage(list(pr.images)[30])
+# pr = ProcessedRun('r11_07_06c')
+# iim = InterfaceImage(list(pr.images)[6])
+
+
+if __name__ == '__main__':
+    test_interface()
