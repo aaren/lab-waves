21f33fc3374aff4e5a24cdea2b4b8b8a75aa0eff
Aaron O'Leary <eeaol@leeds.ac.uk>
2013-09-19 17:48:30 +0100

    plotting class

--- scripts/dostuff.py
+++ scripts/dostuff.py
@@ -51,21 +51,15 @@ def command_line_parser():
                         action='append_const',
                         const='stitch',
                         dest='actions')
-    parser.add_argument('--hovmoller',
-                        help='create hovmoller',
-                        action='append_const',
-                        const='hovmoller',
-                        dest='actions')
-    parser.add_argument('--wave_hovmoller',
-                        help='create wave hovmoller',
-                        action='append_const',
-                        const='wave_hovmoller',
-                        dest='actions')
     parser.add_argument('--cache',
                         help='extract interfaces and save to cache',
                         action='append_const',
                         const='cache',
                         dest='actions')
+    parser.add_argument('--plots',
+                        help='plots to create',
+                        nargs='*',
+                        default=[])
     parser.add_argument('--list',
                         help='list the runs that match run_pattern',
                         action='store_true')
@@ -78,7 +72,6 @@ def main(args):
     logging.basicConfig(filename='logfile', level=logging.DEBUG)
     action_table = {'raw_process': raw_process,
                     'stitch':      stitch,
-                    'hovmoller':   hovmoller,
                     'cache':       cache,
                     }
 
@@ -102,16 +95,21 @@ def main(args):
                 logging.warning(logmsg(run=run, action=action, error=e))
         i += 1
 
-
-def cache(run):
-    ra = RunAnalysis(run, load_from_cache=False)
-    ra.save_to_cache()
+        ra = RunPlotter(run)
+        for plot in args.plots:
+            print "plotting {}".format(plot)
+            getattr(ra, plot)()
 
 
 def get_runs(regex="r1[123]*"):
     runpaths = glob.glob(os.path.join(config.path, 'synced', regex))
     runs = [os.path.basename(p) for p in runpaths]
     return runs
+
+
+def cache(run):
+    ra = RunAnalysis(run, load_from_cache=False)
+    ra.save_to_cache()
 
 
 def stitch(run):
@@ -124,118 +122,12 @@ def raw_process(run):
     r.process()
 
 
-def hovmoller_current(pr, ax):
-    Xg, Tg, Yg = pr.combine_current
-    D = pr.parameters['D']
-    contour_current = dict(cmap=plt.cm.bone_r,
-                            levels=np.linspace(0, D / 2, 100))
-
-    contourf = ax.contourf(Xg, Tg, Yg, **contour_current)
-    return contourf
-
-
-def hovmoller_wave(pr, ax):
-    Xw, Tw, Yw = pr.combine_wave
-    contour_wave = dict(cmap=plt.cm.bone_r,
-                        levels=np.linspace(-1, 1, 100))
-
-    Yw_ = Yw / Yw.mean() - 1
-    contourf = ax.contourf(Xw, Tw, Yw_, **contour_wave)
-    return contourf
-
-
-def hovmoller(run, visible_regions=False):
-    pr = ProcessedRun(run)
-    # FIXME: catch single layer runs and do something else.
-
-    fig, axes = plt.subplots(nrows=2, figsize=(8, 12), dpi=100)
-
-    current_ax = axes[0]
-    wave_ax = axes[1]
-
-    c_wave = hovmoller_wave(pr, wave_ax)
-    c_current = hovmoller_current(pr, current_ax)
-    fig.colorbar(c_wave, ax=wave_ax)
-    fig.colorbar(c_current, ax=current_ax)
-
-    for ax in axes:
-        if visible_regions:
-        # vertical lines where the measurement regions are
-            for i, v in enumerate(vis):
-                ax.axvspan(*v, color='r', alpha=0.2)
-
-            # for i, v in enumerate(vis_):
-                # ax.axvspan(*v, color='y', alpha=0.2)
-
-    # line at the transition
-        ax.axvline(2.5, color='y', linewidth=2)
-        ax.text(2.505, 6, 'phase transition', color='y', rotation=90)
-
-    # label the current viewing region
-        ax.text(2.8, 7, 'test run viewing window', color='w', rotation=90)
-
-        ax.set_xlim(0.0, 3.5)
-        ax.set_xlabel('distance from lock (m)')
-        ax.set_ylabel('time from release (s)')
-
-    title = ("Hovmoller, {run_index} \n"
-             r"$D={D}$, $H={H}$, $h_1={h_1}$, "
-             r"$\rho_0={rho_0}$, $\rho_1={rho_1}$, $\alpha={alpha}$")
-
-    axes[0].set_title(title.format(**pr.parameters))
-
-    fig.tight_layout()
-
-    if visible_regions:
-        fig.savefig('plots/hovmoller_visible_simple_' + run + '.png')
-    else:
-        fig.savefig('plots/hovmoller_combine_' + run + '.png')
-
-    plt.close(fig)
-
-
-def composite(run, visible_regions=False, h=0.13, tag='50mm'):
-    pr = ProcessedRun(run)
-    X, T, Y = pr.combine_current
-
-    fig, ax = plt.subplots()
-
-    for i in range(Y.shape[0]):
-        ax.plot(X[i], Y[i], 'k', alpha=0.7)
-
-    if visible_regions:
-        for v in vis:
-            w = v[1] - v[0]
-            ax.add_patch(mpl.patches.Rectangle((v[0], 0),
-                                               width=w,
-                                               height=h,
-                                               color='r',
-                                               alpha=0.5))
-        for v in vis_:
-            w = v[1] - v[0]
-            ax.add_patch(mpl.patches.Rectangle((v[0], 0),
-                                               width=w,
-                                               height=h,
-                                               color='y',
-                                               alpha=0.5))
-
-    ax.set_xlim(0.0, 3.5)
-    ax.set_ylim(0.0, 0.25)
-
-    ax.set_xlabel('distance (m)')
-    ax.set_ylabel('height (m)')
-
-    if visible_regions:
-        fig.savefig('plots/composite_visible_' + tag + '_' + run + '.png')
-    else:
-        fig.savefig('plots/composite_' + tag + '_' + run + '.png')
-
-
 class RunAnalysis(object):
-    """Container for analysis methods. Also implements caching of
-    interface data."""
+    """Container for basic analysis methods. Also implements caching
+    of interface data."""
     def __init__(self, index, load_from_cache=True):
         self.pr = ProcessedRun(index)
+        self.parameters = type('parameters', (object,), self.pr.parameters)
 
         self.cache_path = os.path.join(self.pr.output_dir, 'cache.npz')
 
@@ -288,6 +180,117 @@ class RunAnalysis(object):
     @combine_current.setter
     def combine_current(self, value):
         self._combine_current = value
+
+
+class RunPlotter(RunAnalysis):
+    """Container for plots."""
+    def hovmoller_current(self, ax):
+        Xg, Tg, Yg = self.combine_current
+        D = self.parameters.D
+        contour_current = dict(cmap=plt.cm.bone_r,
+                                levels=np.linspace(0, D / 2, 100))
+
+        contourf = ax.contourf(Xg, Tg, Yg, **contour_current)
+        return contourf
+
+    def hovmoller_wave(self, ax):
+        Xw, Tw, Yw = self.combine_wave
+        contour_wave = dict(cmap=plt.cm.bone_r,
+                            levels=np.linspace(-1, 1, 100))
+
+        Yw_ = Yw / Yw.mean() - 1
+        contourf = ax.contourf(Xw, Tw, Yw_, **contour_wave)
+        return contourf
+
+    def hovmoller(self, savefig=False, visible_regions=False):
+        # FIXME: catch single layer runs and do something else.
+
+        fig, axes = plt.subplots(nrows=2, figsize=(8, 12), dpi=100)
+
+        current_ax = axes[0]
+        wave_ax = axes[1]
+
+        c_wave = self.hovmoller_wave(wave_ax)
+        c_current = self.hovmoller_current(current_ax)
+        fig.colorbar(c_wave, ax=wave_ax)
+        fig.colorbar(c_current, ax=current_ax)
+
+        for ax in axes:
+            if visible_regions:
+            # vertical lines where the measurement regions are
+                for i, v in enumerate(vis):
+                    ax.axvspan(*v, color='r', alpha=0.2)
+
+                # for i, v in enumerate(vis_):
+                    # ax.axvspan(*v, color='y', alpha=0.2)
+
+        # line at the transition
+            ax.axvline(2.5, color='y', linewidth=2)
+            ax.text(2.505, 6, 'phase transition', color='y', rotation=90)
+
+        # label the current viewing region
+            ax.text(2.8, 7, 'test run viewing window', color='w', rotation=90)
+
+            ax.set_xlim(0.0, 3.5)
+            ax.set_xlabel('distance from lock (m)')
+            ax.set_ylabel('time from release (s)')
+
+        title = ("Hovmoller, {run_index} \n"
+                 r"$D={D}$, $H={H}$, $h_1={h_1}$, "
+                 r"$\rho_0={rho_0}$, $\rho_1={rho_1}$, $\alpha={alpha}$")
+
+        axes[0].set_title(title.format(**self.pr.parameters))
+
+        fig.tight_layout()
+
+        run = self.parameters.run_index
+
+        if savefig:
+            if visible_regions:
+                fig.savefig('plots/hovmoller_visible_simple_' + run + '.png')
+            else:
+                fig.savefig('plots/hovmoller_combine_' + run + '.png')
+
+            plt.close(fig)
+        else:
+            return fig
+
+    def composite(self, visible_regions=False, h=0.13, tag='50mm'):
+        X, T, Y = self.combine_current
+
+        fig, ax = plt.subplots()
+
+        for i in range(Y.shape[0]):
+            ax.plot(X[i], Y[i], 'k', alpha=0.7)
+
+        if visible_regions:
+            for v in vis:
+                w = v[1] - v[0]
+                ax.add_patch(mpl.patches.Rectangle((v[0], 0),
+                                                   width=w,
+                                                   height=h,
+                                                   color='r',
+                                                   alpha=0.5))
+            for v in vis_:
+                w = v[1] - v[0]
+                ax.add_patch(mpl.patches.Rectangle((v[0], 0),
+                                                   width=w,
+                                                   height=h,
+                                                   color='y',
+                                                   alpha=0.5))
+
+        ax.set_xlim(0.0, 3.5)
+        ax.set_ylim(0.0, 0.25)
+
+        ax.set_xlabel('distance (m)')
+        ax.set_ylabel('height (m)')
+
+        run = self.parameters.run_index
+
+        if visible_regions:
+            fig.savefig('plots/composite_visible_' + tag + '_' + run + '.png')
+        else:
+            fig.savefig('plots/composite_' + tag + '_' + run + '.png')
 
 
 if __name__ == '__main__':
