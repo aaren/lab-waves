83dc9c22d02fd558250973385810245afe016603
Aaron O'Leary <aaron.oleary@gmail.com>
2015-01-28 13:43:05 +0000

    write package __init__

---
+++ labwaves/__init__.py
@@ -0,0 +1,9 @@
+import config
+import runbase
+import processing
+
+from .runbase import RawImage
+from .runbase import RawRun
+
+from .runbase import ProcessedImage
+from .runbase import ProcessedRun
