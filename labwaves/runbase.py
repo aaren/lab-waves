4abc77e2c6becfeeb2c23272ee156ad4bd520dcc
Aaron O'Leary <aaron.oleary@gmail.com>
2015-02-06 17:28:03 +0000

    don't overwrite by default

--- labwaves/runbase.py
+++ labwaves/runbase.py
@@ -1463,7 +1463,7 @@ class Run(object):
         self.waves = Waves(*waves)
         self.current = Current(*current)
 
-    def save(self, overwrite=True):
+    def save(self, overwrite=False):
         """Save data to run-wide hdf5"""
         if overwrite and os.path.exists(self.h5name):
             os.remove(self.h5name)
